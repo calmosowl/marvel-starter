1eb97f58d09345b4bd6f9a461ab98a4e56f1ef61
Artem Kyselov <calmos.owl@gmail.com>
2023-02-01 14:32:48 +0200

    feat: implement CharInfo

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -28,11 +28,13 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+        id: char.id,
         name: char.name,
         description: char.description ? truncateString(char.description, 180) : 'We have no data for this character',
         thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
         homepage: char.urls[0].url,
-        wiki: char.urls[1].url
+        wiki: char.urls[1].url,
+        comics: char.comics.items
     }
   }
 }
